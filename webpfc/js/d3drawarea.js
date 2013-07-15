6bb19368ed1e7c2c8df838ad5af1cde471b285bd
stevenyesz <lightning_heat@163.com>
2013-07-15 14:42:39 +0800

    add left panel

--- webpfc/js/d3drawarea.js
+++ webpfc/js/d3drawarea.js
@@ -3,7 +3,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 200 - margin.left - margin.right,
     height = 160 - margin.top - margin.bottom;
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#chart").append("svg")
 .attr("class", "chart")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
